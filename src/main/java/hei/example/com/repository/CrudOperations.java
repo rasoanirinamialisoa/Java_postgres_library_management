8016ccfd71e84c9f3d89ddc8837f28f06f843314
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2023-11-19 11:09:28 +0300

    Added Generic CrudOperations Interface in Repository Package

---
+++ src/main/java/hei/example/com/repository/CrudOperations.java
@@ -0,0 +1,8 @@
+package hei.example.com.repository;
+import java.util.List;
+public interface CrudOperations<T> {
+    List<T> findAll();
+    List<T> saveAll(List<T> toSave);
+    T save(T toSave);
+    T delete(T toDelete);
+}
